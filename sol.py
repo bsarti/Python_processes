5ab4a9d2772af79e8a3b17ee5fd8646668535c12
Benjamin Sarti <benjamin.sarti@famoco.com>
2017-06-24 10:52:15 +1200

    woring for the first 4 lines of the .data file

--- sol.py
+++ sol.py
@@ -1,14 +1,13 @@
- 
+import numpy as np
+import pandas as pd
 
 def main():
-	X = 4
-	t_p = [];
-	time = 0
-	dp = (3, 7)
+	calculate_time_file('proc_disk_usage.data')
+
+def calculate_print_time(X, dp):
+	time = 0 
 	n = len(dp)
 	dp_count = [i for i in dp]
-	
-
 	while (X>0):
 		min_dp_count = min(dp_count)
 		dp_count = [i - min_dp_count for i in dp_count]
@@ -21,5 +20,19 @@ def main():
 		X = X - bytes_used
 	print(time)
 
+def calculate_time_file(filename):
+	data = []
+	with open(filename) as f:
+		data_string = f.readlines()
+		data_string = [x.strip() for x in data_string] #separate data per line
+		data_integers = [stringofints.split() for stringofints in data_string]
+	for i in xrange(len(data_integers)):
+		datatxt_integers = [int(numeric_string) for numeric_string in data_integers[i]]
+		data.append(datatxt_integers)
+	for i in xrange(len(data)):
+		X = data[i][0]
+		dp = data[i][1:] 
+		calculate_print_time(X, dp)
+
 if __name__ == '__main__':
-    main()
+	main()
